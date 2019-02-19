2c9176773a66617b7e74102a79aff5719f7bd850
Alex Suevalov <alex.suevalov@contentful.com>
2019-02-19 13:39:09 +0100

    Added inlining both CSS and JS

--- samples/publish-confirm/build.js
+++ samples/publish-confirm/build.js
@@ -2,13 +2,29 @@ const fs = require('fs');
 const path = require('path');
 
 const ENC = { encoding: 'utf8' };
-const TAG_TO_REPLACE = '<script type="text/javascript" src="./index.jsx"></script>';
+const SCRIPT_PLACEHOLDER = '<placeholder>SCRIPT_PLACEHOLDER</placeholder>';
+const STYLES_PLACEHOLDER = '<placeholder>STYLES_PLACEHOLDER</placeholder>'
 
 const resolve = file => path.resolve(__dirname, file);
 const read = file => fs.readFileSync(resolve(file), ENC);
 
-const html = read('./src/index.html');
-const script = ['<script>', read('./build/index.js'), '</script>'].join('\n');
-const result = html.replace(TAG_TO_REPLACE, script);
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <style type="text/css">${STYLES_PLACEHOLDER}</style>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="text/javascript">${SCRIPT_PLACEHOLDER}</script>
+  </body>
+</html>
+`;
+
+let result = html;
+
+result = result.replace(SCRIPT_PLACEHOLDER, read('./build/index.js'));
+result = result.replace(STYLES_PLACEHOLDER, read('./build/index.css'));
 
 fs.writeFileSync(resolve('./build/index.html'), result, ENC);
